bb0076634f0916379928304517f0866ee0ec0ed3
Scott Williams <tocsoft@gmail.com>
2017-01-17 08:20:17 +0000

    :cop:

--- src/Shaper2D/IPath.cs
+++ src/Shaper2D/IPath.cs
@@ -3,10 +3,8 @@
 // Licensed under the Apache License, Version 2.0.
 // </copyright>
 
-
 namespace Shaper2D
 {
-    using System.Numerics;
     using System.Collections.Immutable;
 
     /// <summary>
