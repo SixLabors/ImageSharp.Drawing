7a0b1ecacaacd704a53a134a212cf34024912895
Scott Williams <tocsoft@gmail.com>
2017-01-28 18:41:37 +0000

    fix codecov

--- tests/CodeCoverage/CodeCoverage.cmd
+++ tests/CodeCoverage/CodeCoverage.cmd
@@ -4,7 +4,7 @@ cd tests\CodeCoverage
 
 nuget restore packages.config -PackagesDirectory .
 
-cd ..\Shaper2D.Tests
+cd ..\SixLabors.Shapes.Tests
 
 dotnet restore
 
@@ -12,10 +12,10 @@ cd ..
 cd ..
 
 rem The -threshold options prevents this taking ages...
-tests\CodeCoverage\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:"test tests\Shaper2D.Tests -c Release -f net451" -threshold:10 -register:user -filter:"+[Shaper2D*]*" -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:All -returntargetcode -output:.\Shaper2D.Coverage.xml
+tests\CodeCoverage\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:"test tests\SixLabors.Shapes.Tests -c Release -f net451" -threshold:10 -register:user -filter:"+[SixLabors.Shapes*]*" -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:All -returntargetcode -output:.\SixLabors.Shapes.Coverage.xml
 
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 SET PATH=C:\\Python34;C:\\Python34\\Scripts;%PATH%
 pip install codecov
-codecov -f "Shaper2D.Coverage.xml"
+codecov -f "SixLabors.Shapes.Coverage.xml"
