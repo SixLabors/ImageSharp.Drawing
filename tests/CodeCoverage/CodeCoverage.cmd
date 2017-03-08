8d1bcc98a8a83523c6cb1a2ea0a04c961cb9e8a5
Scott Williams <tocsoft@gmail.com>
2017-03-08 17:16:18 +0000

    use full pdb for codecov

--- tests/CodeCoverage/CodeCoverage.cmd
+++ tests/CodeCoverage/CodeCoverage.cmd
@@ -7,13 +7,13 @@ nuget restore packages.config -PackagesDirectory .
 cd ..\SixLabors.Shapes.Tests
 
 dotnet restore SixLabors.Shapes.Tests.csproj
-dotnet build SixLabors.Shapes.Tests.csproj /p:codecov=true
+dotnet build SixLabors.Shapes.Tests.csproj -c Release /p:codecov=true
 
 cd ..
 cd ..
 
 rem The -threshold options prevents this taking ages...
-tests\CodeCoverage\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"dotnet.exe" -targetargs:"test tests\SixLabors.Shapes.Tests\SixLabors.Shapes.Tests.csproj --no-build -c Release" -threshold:10 -register:user -filter:"+[SixLabors.Shapes*]*" -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:All -returntargetcode -output:.\SixLabors.Shapes.Coverage.xml
+tests\CodeCoverage\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"dotnet.exe" -targetargs:"test tests\SixLabors.Shapes.Tests\SixLabors.Shapes.Tests.csproj --no-build -c Release /p:codecov=true" -threshold:10 -register:user -filter:"+[SixLabors.Shapes*]*" -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:All -returntargetcode -output:.\SixLabors.Shapes.Coverage.xml
 
 if %errorlevel% neq 0 exit /b %errorlevel%
 
