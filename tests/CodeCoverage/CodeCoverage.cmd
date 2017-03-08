41ce5a38b8b3b74bd3c967286b6bf7c2f702143c
Scott Williams <tocsoft@gmail.com>
2017-03-08 16:59:27 +0000

    fixes to codecov

--- tests/CodeCoverage/CodeCoverage.cmd
+++ tests/CodeCoverage/CodeCoverage.cmd
@@ -7,12 +7,13 @@ nuget restore packages.config -PackagesDirectory .
 cd ..\SixLabors.Shapes.Tests
 
 dotnet restore SixLabors.Shapes.Tests.csproj
+dotnet build SixLabors.Shapes.Tests.csproj /p:codecov=true
 
 cd ..
 cd ..
 
 rem The -threshold options prevents this taking ages...
-tests\CodeCoverage\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"C:\Users\appveyor\AppData\Local\Microsoft\dotnet\dotnet.exe" -targetargs:"test tests\SixLabors.Shapes.Tests\SixLabors.Shapes.Tests.csproj -c Release -f net451" -threshold:10 -register:user -filter:"+[SixLabors.Shapes*]*" -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:All -returntargetcode -output:.\SixLabors.Shapes.Coverage.xml
+tests\CodeCoverage\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"dotnet.exe" -targetargs:"test tests\SixLabors.Shapes.Tests\SixLabors.Shapes.Tests.csproj --no-build -c Release" -threshold:10 -register:user -filter:"+[SixLabors.Shapes*]*" -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:All -returntargetcode -output:.\SixLabors.Shapes.Coverage.xml
 
 if %errorlevel% neq 0 exit /b %errorlevel%
 
