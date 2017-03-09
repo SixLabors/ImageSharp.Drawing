2efe071aed28841553b814f647c0e1f915a7f651
Scott Williams <tocsoft@gmail.com>
2017-03-09 19:05:38 +0000

    consolidate packages

--- src/ImageSharp.Drawing/Paths/DrawRectangle.cs
+++ src/ImageSharp.Drawing/Paths/DrawRectangle.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="DrawPath.cs" company="James Jackson-South">
+﻿// <copyright file="DrawRectangle.cs" company="James Jackson-South">
 // Copyright (c) James Jackson-South and contributors.
 // Licensed under the Apache License, Version 2.0.
 // </copyright>
@@ -10,7 +10,6 @@ namespace ImageSharp
     using Drawing;
     using Drawing.Brushes;
     using Drawing.Pens;
-    using Drawing.Processors;
 
     /// <summary>
     /// Extension methods for the <see cref="Image{TColor}"/> type.
@@ -18,18 +17,18 @@ namespace ImageSharp
     public static partial class ImageExtensions
     {
         /// <summary>
-        /// Draws the outline of the region with the provided pen.
+        /// Draws the outline of the polygon with the provided pen.
         /// </summary>
         /// <typeparam name="TColor">The type of the color.</typeparam>
         /// <param name="source">The image this method extends.</param>
         /// <param name="pen">The pen.</param>
-        /// <param name="path">The path.</param>
+        /// <param name="shape">The shape.</param>
         /// <param name="options">The options.</param>
         /// <returns>The <see cref="Image{TColor}"/>.</returns>
-        public static Image<TColor> Draw<TColor>(this Image<TColor> source, IPen<TColor> pen, Drawable path, GraphicsOptions options)
+        public static Image<TColor> Draw<TColor>(this Image<TColor> source, IPen<TColor> pen, Rectangle shape, GraphicsOptions options)
            where TColor : struct, IPixel<TColor>
         {
-            return source.Apply(new DrawPathProcessor<TColor>(pen, path, options));
+            return source.Draw(pen, new SixLabors.Shapes.Rectangle(shape.X, shape.Y, shape.Width, shape.Height), options);
         }
 
         /// <summary>
@@ -38,12 +37,12 @@ namespace ImageSharp
         /// <typeparam name="TColor">The type of the color.</typeparam>
         /// <param name="source">The image this method extends.</param>
         /// <param name="pen">The pen.</param>
-        /// <param name="path">The path.</param>
+        /// <param name="shape">The shape.</param>
         /// <returns>The <see cref="Image{TColor}"/>.</returns>
-        public static Image<TColor> Draw<TColor>(this Image<TColor> source, IPen<TColor> pen, Drawable path)
+        public static Image<TColor> Draw<TColor>(this Image<TColor> source, IPen<TColor> pen, Rectangle shape)
            where TColor : struct, IPixel<TColor>
         {
-            return source.Draw(pen, path, GraphicsOptions.Default);
+            return source.Draw(pen, shape, GraphicsOptions.Default);
         }
 
         /// <summary>
@@ -53,13 +52,13 @@ namespace ImageSharp
         /// <param name="source">The image this method extends.</param>
         /// <param name="brush">The brush.</param>
         /// <param name="thickness">The thickness.</param>
-        /// <param name="path">The path.</param>
+        /// <param name="shape">The shape.</param>
         /// <param name="options">The options.</param>
         /// <returns>The <see cref="Image{TColor}"/>.</returns>
-        public static Image<TColor> Draw<TColor>(this Image<TColor> source, IBrush<TColor> brush, float thickness, Drawable path, GraphicsOptions options)
+        public static Image<TColor> Draw<TColor>(this Image<TColor> source, IBrush<TColor> brush, float thickness, Rectangle shape, GraphicsOptions options)
            where TColor : struct, IPixel<TColor>
         {
-            return source.Draw(new Pen<TColor>(brush, thickness), path, options);
+            return source.Draw(new Pen<TColor>(brush, thickness), shape, options);
         }
 
         /// <summary>
@@ -69,12 +68,12 @@ namespace ImageSharp
         /// <param name="source">The image this method extends.</param>
         /// <param name="brush">The brush.</param>
         /// <param name="thickness">The thickness.</param>
-        /// <param name="path">The path.</param>
+        /// <param name="shape">The shape.</param>
         /// <returns>The <see cref="Image{TColor}"/>.</returns>
-        public static Image<TColor> Draw<TColor>(this Image<TColor> source, IBrush<TColor> brush, float thickness, Drawable path)
+        public static Image<TColor> Draw<TColor>(this Image<TColor> source, IBrush<TColor> brush, float thickness, Rectangle shape)
            where TColor : struct, IPixel<TColor>
         {
-            return source.Draw(new Pen<TColor>(brush, thickness), path);
+            return source.Draw(new Pen<TColor>(brush, thickness), shape);
         }
 
         /// <summary>
@@ -84,13 +83,13 @@ namespace ImageSharp
         /// <param name="source">The image this method extends.</param>
         /// <param name="color">The color.</param>
         /// <param name="thickness">The thickness.</param>
-        /// <param name="path">The path.</param>
+        /// <param name="shape">The shape.</param>
         /// <param name="options">The options.</param>
         /// <returns>The <see cref="Image{TColor}"/>.</returns>
-        public static Image<TColor> Draw<TColor>(this Image<TColor> source, TColor color, float thickness, Drawable path, GraphicsOptions options)
+        public static Image<TColor> Draw<TColor>(this Image<TColor> source, TColor color, float thickness, Rectangle shape, GraphicsOptions options)
            where TColor : struct, IPixel<TColor>
         {
-            return source.Draw(new SolidBrush<TColor>(color), thickness, path, options);
+            return source.Draw(new SolidBrush<TColor>(color), thickness, shape, options);
         }
 
         /// <summary>
@@ -100,12 +99,12 @@ namespace ImageSharp
         /// <param name="source">The image this method extends.</param>
         /// <param name="color">The color.</param>
         /// <param name="thickness">The thickness.</param>
-        /// <param name="path">The path.</param>
+        /// <param name="shape">The shape.</param>
         /// <returns>The <see cref="Image{TColor}"/>.</returns>
-        public static Image<TColor> Draw<TColor>(this Image<TColor> source, TColor color, float thickness, Drawable path)
+        public static Image<TColor> Draw<TColor>(this Image<TColor> source, TColor color, float thickness, Rectangle shape)
            where TColor : struct, IPixel<TColor>
         {
-            return source.Draw(new SolidBrush<TColor>(color), thickness, path);
+            return source.Draw(new SolidBrush<TColor>(color), thickness, shape);
         }
     }
 }
