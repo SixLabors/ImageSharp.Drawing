1a89e858c638e1bb34f88c5563092098e5813008
Scott Williams <tocsoft@gmail.com>
2017-05-30 22:26:02 +0100

    specify text location

--- src/SixLabors.Shapes.Text/TextBuilder.cs
+++ src/SixLabors.Shapes.Text/TextBuilder.cs
@@ -7,7 +7,7 @@ using System.Text;
 
 namespace SixLabors.Shapes
 {
-	/// <summary>
+    /// <summary>
     /// Text drawing extensions for a PathBuilder
     /// </summary>
     public static class TextBuilder
@@ -16,17 +16,29 @@ namespace SixLabors.Shapes
         /// Generates the shapes corresponding the glyphs described by the font and with the setting ing withing the FontSpan
         /// </summary>
         /// <param name="text">The text to generate glyphs for</param>
+        /// <param name="location">The location</param>
         /// <param name="style">The style and settings to use while rendering the glyphs</param>
         /// <returns></returns>
-        public static IPathCollection GenerateGlyphs(string text, FontSpan style)
+        public static IPathCollection GenerateGlyphs(string text, Vector2 location, FontSpan style)
         {
-            var glyphBuilder = new GlyphBuilder();
+            var glyphBuilder = new GlyphBuilder(location);
 
             TextRenderer renderer = new TextRenderer(glyphBuilder);
 
             renderer.RenderText(text, style);
 
             return new PathCollection(glyphBuilder.Paths);
+        }
+
+        /// <summary>
+        /// Generates the shapes corresponding the glyphs described by the font and with the setting ing withing the FontSpan
+        /// </summary>
+        /// <param name="text">The text to generate glyphs for</param>
+        /// <param name="style">The style and settings to use while rendering the glyphs</param>
+        /// <returns></returns>
+        public static IPathCollection GenerateGlyphs(string text, FontSpan style)
+        {
+            return GenerateGlyphs(text, Vector2.Zero, style);
         }
     }
 
