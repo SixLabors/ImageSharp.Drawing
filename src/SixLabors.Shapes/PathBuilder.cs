20d3257a3bf1565055205d89f98c9b9a2da062a0
Scott Williams <tocsoft@gmail.com>
2017-06-10 16:46:50 +0100

    Make PathBuilder chainable

--- src/SixLabors.Shapes/PathBuilder.cs
+++ src/SixLabors.Shapes/PathBuilder.cs
@@ -15,7 +15,7 @@ namespace SixLabors.Shapes
     /// <summary>
     /// Allow you to derivatively build shapes and paths.
     /// </summary>
-    public class PathBuilder
+    public class PathBuilder 
     {
         private readonly List<Figure> figures = new List<Figure>();
         private readonly Matrix3x2 defaultTransform;
@@ -46,39 +46,46 @@ namespace SixLabors.Shapes
         /// Sets the translation to be applied to all items to follow being applied to the <see cref="PathBuilder"/>.
         /// </summary>
         /// <param name="translation">The translation.</param>
-        public void SetTransform(Matrix3x2 translation)
+        public PathBuilder SetTransform(Matrix3x2 translation)
         {
             this.setTransform = translation;
             this.currentTransform = this.setTransform * this.defaultTransform;
+            return this;
         }
 
         /// <summary>
         /// Sets the origin all subsequent point should be relative to.
         /// </summary>
         /// <param name="origin">The origin.</param>
-        public void SetOrigin(PointF origin)
+        public PathBuilder SetOrigin(PointF origin)
         {
             // the new origin should be transofrmed based on the default transform
             this.setTransform.Translation = origin;
             this.currentTransform = this.setTransform * this.defaultTransform;
+
+            return this;
         }
 
         /// <summary>
         /// Resets the translation to the default.
         /// </summary>
-        public void ResetTransform()
+        public PathBuilder ResetTransform()
         {
             this.setTransform = Matrix3x2.Identity;
             this.currentTransform = this.setTransform * this.defaultTransform;
+
+            return this;
         }
 
         /// <summary>
         /// Resets the origin to the default.
         /// </summary>
-        public void ResetOrigin()
+        public PathBuilder ResetOrigin()
         {
             this.setTransform.Translation = Vector2.Zero;
             this.currentTransform = this.setTransform * this.defaultTransform;
+
+            return this;
         }
 
         /// <summary>
@@ -86,11 +93,13 @@ namespace SixLabors.Shapes
         /// </summary>
         /// <param name="start">The start.</param>
         /// <param name="end">The end.</param>
-        public void AddLine(PointF start, PointF end)
+        public PathBuilder AddLine(PointF start, PointF end)
         {
             end = PointF.Transform(end, this.currentTransform);
             start = PointF.Transform(start, this.currentTransform);
             this.currentFigure.AddSegment(new LinearLineSegment(start, end));
+
+            return this;
         }
 
         /// <summary>
@@ -100,38 +109,46 @@ namespace SixLabors.Shapes
         /// <param name="y1">The y1.</param>
         /// <param name="x2">The x2.</param>
         /// <param name="y2">The y2.</param>
-        public void AddLine(float x1, float y1, float x2, float y2)
+        public PathBuilder AddLine(float x1, float y1, float x2, float y2)
         {
             this.AddLine(new PointF(x1, y1), new PointF(x2, y2));
+
+            return this;
         }
 
         /// <summary>
         /// Adds a series of line segments connecting the current point to the new points.
         /// </summary>
         /// <param name="points">The points.</param>
-        public void AddLines(IEnumerable<PointF> points)
+        public PathBuilder AddLines(IEnumerable<PointF> points)
         {
             Guard.NotNull(points, nameof(points));
 
             this.AddLines(points.ToArray());
+
+            return this;
         }
 
         /// <summary>
         /// Adds a series of line segments connecting the current point to the new points.
         /// </summary>
         /// <param name="points">The points.</param>
-        public void AddLines(params PointF[] points)
+        public PathBuilder AddLines(params PointF[] points)
         {
             this.AddSegment(new LinearLineSegment(points));
+
+            return this;
         }
 
         /// <summary>
         /// Adds the segment.
         /// </summary>
         /// <param name="segment">The segment.</param>
-        public void AddSegment(ILineSegment segment)
+        public PathBuilder AddSegment(ILineSegment segment)
         {
             this.currentFigure.AddSegment(segment.Transform(this.currentTransform));
+
+            return this;
         }
 
         /// <summary>
@@ -140,7 +157,7 @@ namespace SixLabors.Shapes
         /// <param name="startPoint">The start point.</param>
         /// <param name="controlPoint">The control point1.</param>
         /// <param name="endPoint">The end point.</param>
-        public void AddBezier(PointF startPoint, PointF controlPoint, PointF endPoint)
+        public PathBuilder AddBezier(PointF startPoint, PointF controlPoint, PointF endPoint)
         {
             Vector2 startPointVector = startPoint;
             Vector2 controlPointVector = controlPoint;
@@ -150,6 +167,8 @@ namespace SixLabors.Shapes
             Vector2 c2 = (((controlPointVector - endPointVector) * 2) / 3) + endPointVector;
 
             this.AddBezier(startPointVector, c1, c2, endPoint);
+
+            return this;
         }
 
         /// <summary>
@@ -159,19 +178,21 @@ namespace SixLabors.Shapes
         /// <param name="controlPoint1">The control point1.</param>
         /// <param name="controlPoint2">The control point2.</param>
         /// <param name="endPoint">The end point.</param>
-        public void AddBezier(PointF startPoint, PointF controlPoint1, PointF controlPoint2, PointF endPoint)
+        public PathBuilder AddBezier(PointF startPoint, PointF controlPoint1, PointF controlPoint2, PointF endPoint)
         {
             this.currentFigure.AddSegment(new CubicBezierLineSegment(
                 PointF.Transform(startPoint, this.currentTransform),
                 PointF.Transform(controlPoint1, this.currentTransform),
                 PointF.Transform(controlPoint2, this.currentTransform),
                 PointF.Transform(endPoint, this.currentTransform)));
+
+            return this;
         }
 
         /// <summary>
         /// Starts a new figure but leaves the previous one open.
         /// </summary>
-        public void StartFigure()
+        public PathBuilder StartFigure()
         {
             if (!this.currentFigure.IsEmpty)
             {
@@ -182,21 +203,25 @@ namespace SixLabors.Shapes
             {
                 this.currentFigure.IsClosed = false;
             }
+
+            return this;
         }
 
         /// <summary>
         /// Closes the current figure.
         /// </summary>
-        public void CloseFigure()
+        public PathBuilder CloseFigure()
         {
             this.currentFigure.IsClosed = true;
             this.StartFigure();
+
+            return this;
         }
 
         /// <summary>
         /// Closes the current figure.
         /// </summary>
-        public void CloseAllFigures()
+        public PathBuilder CloseAllFigures()
         {
             foreach (Figure f in this.figures)
             {
@@ -204,6 +229,8 @@ namespace SixLabors.Shapes
             }
 
             this.CloseFigure();
+
+            return this;
         }
 
         /// <summary>
@@ -224,10 +251,12 @@ namespace SixLabors.Shapes
         /// <summary>
         /// Resets this instance, clearing any drawn paths and reseting any transforms.
         /// </summary>
-        public void Reset()
+        public PathBuilder Reset()
         {
             this.Clear();
             this.ResetTransform();
+
+            return this;
         }
 
         /// <summary>
