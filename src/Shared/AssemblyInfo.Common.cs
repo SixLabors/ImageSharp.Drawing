d2496b0e27e7d772cc8015e2edf7de660693d85f
Scott Williams <tocsoft@gmail.com>
2020-01-12 13:06:45 +0000

    merge shapes into drawing project

--- src/Shared/AssemblyInfo.Common.cs
+++
@@ -1,8 +0,0 @@
-﻿// Copyright (c) Six Labors and contributors.
-// Licensed under the Apache License, Version 2.0.
-
-using System.Runtime.CompilerServices;
-
-// Ensure the internals can be tested.
-[assembly: InternalsVisibleTo("SixLabors.Shapes.Tests")]
-[assembly: InternalsVisibleTo("SixLabors.Shapes.Benchmarks")]
