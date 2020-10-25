1615360ef080341c8ab9a2658c7687b9c8d750a9
Anton Firszov <antonfir@gmail.com>
2020-10-25 21:27:38 +0100

    simple trick to shave hair from Mississippi

--- src/ImageSharp.Drawing/Shapes/Scan/ScanEdge.cs
+++ src/ImageSharp.Drawing/Shapes/Scan/ScanEdge.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Buffers;
+using System.Numerics;
 using System.Runtime.CompilerServices;
 using SixLabors.ImageSharp.Memory;
 
@@ -17,22 +18,28 @@ namespace SixLabors.ImageSharp.Drawing.Shapes.Scan
     {
         public readonly float Y0;
         public readonly float Y1;
-        public readonly float P;
-        public readonly float Q;
+        private readonly float p;
+        private readonly float q;
+        private readonly Vector2 c;
 
         // Store 3 small values in a single Int32, to make EdgeData more compact:
         // EdgeUp, Emit0, Emit1
         private readonly int flags;
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal ScanEdge(PointF p0, PointF p1, int flags)
+        internal ScanEdge(Vector2 p0, Vector2 p1, int flags)
         {
             this.Y0 = p0.Y;
             this.Y1 = p1.Y;
             this.flags = flags;
             float dy = p1.Y - p0.Y;
-            this.P = (p1.X - p0.X) / dy;
-            this.Q = ((p0.X * p1.Y) - (p1.X * p0.Y)) / dy;
+
+            this.c = (p0 + p1) * 0.5f;
+            p0 -= this.c;
+            p1 -= this.c;
+
+            this.p = (p1.X - p0.X) / dy;
+            this.q = ((p0.X * p1.Y) - (p1.X * p0.Y)) / dy;
         }
 
         // True when non-horizontal edge is oriented upwards in screen coords
@@ -46,9 +53,9 @@ namespace SixLabors.ImageSharp.Drawing.Shapes.Scan
         // When the scanline intersects the endpoint at Y1.
         public int EmitV1 => (this.flags & 0b11000) >> 3;
 
-        public float GetX(float y) => (this.P * y) + this.Q;
+        public float GetX(float y) => (this.p * (y - this.c.Y)) + this.q + this.c.X;
 
         public override string ToString()
-            => $"(Y0={this.Y0} Y1={this.Y1} E0={this.EmitV0} E1={this.EmitV1} {(this.EdgeUp ? "Up" : "Down")} P={this.P} Q={this.Q})";
+            => $"(Y0={this.Y0} Y1={this.Y1} E0={this.EmitV0} E1={this.EmitV1} {(this.EdgeUp ? "Up" : "Down")} P={this.p} Q={this.q})";
     }
 }
