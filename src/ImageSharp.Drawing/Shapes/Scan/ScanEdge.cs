c8a15246211edbc7354ec95d1f1367c4a6fd9e8d
Anton Firszov <antonfir@gmail.com>
2020-10-25 22:24:39 +0100

    Revert "use doubles"
    This reverts commit 069055a6940281646c87d659e658fb6cc5008412.

--- src/ImageSharp.Drawing/Shapes/Scan/ScanEdge.cs
+++ src/ImageSharp.Drawing/Shapes/Scan/ScanEdge.cs
@@ -18,8 +18,8 @@ namespace SixLabors.ImageSharp.Drawing.Shapes.Scan
     {
         public readonly float Y0;
         public readonly float Y1;
-        private readonly double p;
-        private readonly double q;
+        private readonly float p;
+        private readonly float q;
 
         // Store 3 small values in a single Int32, to make EdgeData more compact:
         // EdgeUp, Emit0, Emit1
@@ -31,23 +31,18 @@ namespace SixLabors.ImageSharp.Drawing.Shapes.Scan
             this.Y0 = p0.Y;
             this.Y1 = p1.Y;
             this.flags = flags;
-            double dy = (double)p1.Y - p0.Y;
+            float dy = p1.Y - p0.Y;
 
             // To improve accuracy, center the edge around zero before calculating the coefficients:
-            double cx = ((double)p0.X + p1.X) * 0.5;
-            double cy = ((double)p0.Y + p1.Y) * 0.5;
+            float cx = (p0.X + p1.X) * 0.5f;
+            float cy = (p0.Y + p1.Y) * 0.5f;
+            p0.X -= cx;
+            p0.Y -= cy;
+            p1.X -= cx;
+            p1.Y -= cy;
 
-            // p0.X -= cx;
-            // p0.Y -= cy;
-            // p1.X -= cx;
-            // p1.Y -= cy;
-            double p0x = p0.X - cx;
-            double p0y = p0.Y - cy;
-            double p1x = p1.X - cx;
-            double p1y = p1.Y - cy;
-
-            this.p = (p1x - p0x) / dy;
-            this.q = ((p0x * p1y) - (p1x * p0y)) / dy;
+            this.p = (p1.X - p0.X) / dy;
+            this.q = ((p0.X * p1.Y) - (p1.X * p0.Y)) / dy;
 
             // After centering, the equation would be:
             // x = p * (y-cy) + q + cx
@@ -66,7 +61,7 @@ namespace SixLabors.ImageSharp.Drawing.Shapes.Scan
         // When the scanline intersects the endpoint at Y1.
         public int EmitV1 => (this.flags & 0b11000) >> 3;
 
-        public float GetX(float y) => (float)((this.p * y) + this.q);
+        public float GetX(float y) => (this.p * y) + this.q;
 
         public override string ToString()
             => $"(Y0={this.Y0} Y1={this.Y1} E0={this.EmitV0} E1={this.EmitV1} {(this.EdgeUp ? "Up" : "Down")} p={this.p} q={this.q})";
