c10e957c88372abba6a58adec7adde437ddb86c5
Anton Firszov <antonfir@gmail.com>
2020-10-26 21:02:26 +0100

    LargeGeoJson_Fill

--- tests/ImageSharp.Drawing.Tests/Drawing/DrawingRobustnessTests.cs
+++ tests/ImageSharp.Drawing.Tests/Drawing/DrawingRobustnessTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -15,13 +16,12 @@ namespace SixLabors.ImageSharp.Drawing.Tests.Drawing
     [GroupOutput("Drawing")]
     public class DrawingRobustnessTests
     {
-        
         [Theory]
         // [WithSolidFilledImages(3600, 2400, "Black", PixelTypes.Rgba32, TestImages.GeoJson.States, 16, 30, 30, false)]
         [WithSolidFilledImages(3600, 2400, "Black", PixelTypes.Rgba32, TestImages.GeoJson.States, 16, 30, 30, true)]
         // [WithSolidFilledImages(7200, 4800, "Black", PixelTypes.Rgba32, TestImages.GeoJson.States, 16, 60, 60, false)]
         // [WithSolidFilledImages(7200, 4800, "Black", PixelTypes.Rgba32, TestImages.GeoJson.States, 16, 60, 60, true)]
-        public void LargeGeoJson(TestImageProvider<Rgba32> provider, string geoJsonFile, int aa, float sx, float sy, bool usePolygonScanner)
+        public void LargeGeoJson_Lines(TestImageProvider<Rgba32> provider, string geoJsonFile, int aa, float sx, float sy, bool usePolygonScanner)
         {
             string jsonContent = File.ReadAllText(TestFile.GetInputFileFullPath(geoJsonFile));
 
@@ -35,7 +35,43 @@ namespace SixLabors.ImageSharp.Drawing.Tests.Drawing
             };
             foreach (PointF[] loop in points)
             {
-                image.Mutate(c => DrawLineExtensions.DrawLines(c, options, Color.White, 1.0f, loop));
+                image.Mutate(c => c.DrawLines(options, Color.White, 1.0f, loop));
+            }
+
+            string details = $"_{System.IO.Path.GetFileName(geoJsonFile)}_{sx}x{sy}_aa{aa}";
+            if (usePolygonScanner)
+            {
+                details += "_Scanner";
+            }
+
+            image.DebugSave(provider,
+                details,
+                appendPixelTypeToFileName: false,
+                appendSourceFileOrDescription: false);
+        }
+
+        [Theory]
+        // [WithSolidFilledImages(3600, 2400, "Black", PixelTypes.Rgba32, TestImages.GeoJson.States, 16, 30, 30, false)]
+        [WithSolidFilledImages(3600, 2400, "Black", PixelTypes.Rgba32, TestImages.GeoJson.States, 16, 30, 30, true)]
+        [WithSolidFilledImages(7200, 4800, "Black", PixelTypes.Rgba32, TestImages.GeoJson.States, 16, 60, 60, false)]
+        [WithSolidFilledImages(7200, 4800, "Black", PixelTypes.Rgba32, TestImages.GeoJson.States, 16, 60, 60, true)]
+        public void LargeGeoJson_Fill(TestImageProvider<Rgba32> provider, string geoJsonFile, int aa, float sx, float sy, bool usePolygonScanner)
+        {
+            string jsonContent = File.ReadAllText(TestFile.GetInputFileFullPath(geoJsonFile));
+
+            PointF[][] points = PolygonFactory.GetGeoJsonPoints(jsonContent, Matrix3x2.CreateScale(sx, sy));
+
+            using Image<Rgba32> image = provider.GetImage();
+            var options = new ShapeGraphicsOptions()
+            {
+                GraphicsOptions = new GraphicsOptions() {Antialias = aa > 0, AntialiasSubpixelDepth = aa},
+                ShapeOptions = new ShapeOptions() { UsePolygonScanner = usePolygonScanner}
+            };
+            var rnd = new Random(42);
+            foreach (PointF[] loop in points)
+            {
+                Color color = Color.WebSafePalette.Span[rnd.Next(Color.WebSafePalette.Length)];
+                image.Mutate(c => c.FillPolygon(options, color, loop));
             }
 
             string details = $"_{System.IO.Path.GetFileName(geoJsonFile)}_{sx}x{sy}_aa{aa}";
@@ -52,14 +88,14 @@ namespace SixLabors.ImageSharp.Drawing.Tests.Drawing
 
         [Theory]
         [WithSolidFilledImages(10000, 10000, "Black", PixelTypes.Rgba32, 16, 0, false)]
-        [WithSolidFilledImages(10000, 10000, "Black", PixelTypes.Rgba32, 16, 0, true)]
-        [WithSolidFilledImages(10000, 10000, "Black", PixelTypes.Rgba32, 16, 5000, false)]
-        [WithSolidFilledImages(10000, 10000, "Black", PixelTypes.Rgba32, 16, 5000, true)]
-        [WithSolidFilledImages(10000, 10000, "Black", PixelTypes.Rgba32, 16, 9000, true)]
-        public void Mississippi(TestImageProvider<Rgba32> provider, int aa, int offset, bool usePolygonScanner)
+        // [WithSolidFilledImages(10000, 10000, "Black", PixelTypes.Rgba32, 16, 0, true)]
+        // [WithSolidFilledImages(10000, 10000, "Black", PixelTypes.Rgba32, 16, 5000, false)]
+        // [WithSolidFilledImages(10000, 10000, "Black", PixelTypes.Rgba32, 16, 5000, true)]
+        // [WithSolidFilledImages(10000, 10000, "Black", PixelTypes.Rgba32, 16, 9000, true)]
+        public void Mississippi_Lines(TestImageProvider<Rgba32> provider, int aa, int offset, bool usePolygonScanner)
         {
             string jsonContent = File.ReadAllText(TestFile.GetInputFileFullPath(TestImages.GeoJson.States));
-            
+
             FeatureCollection features = JsonConvert.DeserializeObject<FeatureCollection>(jsonContent);
 
             var missisipiGeom = features.Features.Single(f => (string) f.Properties["NAME"] == "Mississippi").Geometry;
@@ -68,7 +104,7 @@ namespace SixLabors.ImageSharp.Drawing.Tests.Drawing
                             * Matrix3x2.CreateScale(60, 60)
                             * Matrix3x2.CreateTranslation(offset, offset);
             IReadOnlyList<PointF[]> points =PolygonFactory.GetGeoJsonPoints(missisipiGeom, transform);
-            
+
             using Image<Rgba32> image = provider.GetImage();
             var options = new ShapeGraphicsOptions()
             {
@@ -99,7 +135,7 @@ namespace SixLabors.ImageSharp.Drawing.Tests.Drawing
         public void Missisippi_Skia(int offset)
         {
             string jsonContent = File.ReadAllText(TestFile.GetInputFileFullPath(TestImages.GeoJson.States));
-            
+
             FeatureCollection features = JsonConvert.DeserializeObject<FeatureCollection>(jsonContent);
 
             var missisipiGeom = features.Features.Single(f => (string) f.Properties["NAME"] == "Mississippi").Geometry;
@@ -108,8 +144,8 @@ namespace SixLabors.ImageSharp.Drawing.Tests.Drawing
                             * Matrix3x2.CreateScale(60, 60)
                             * Matrix3x2.CreateTranslation(offset, offset);
             IReadOnlyList<PointF[]> points =PolygonFactory.GetGeoJsonPoints(missisipiGeom, transform);
-            
-            
+
+
             SKPath path = new SKPath();
 
             foreach (PointF[] pts in points.Where(p => p.Length > 2))
@@ -122,7 +158,7 @@ namespace SixLabors.ImageSharp.Drawing.Tests.Drawing
                 }
                 path.LineTo(pts[0].X, pts[0].Y);
             }
-            
+
             SKImageInfo imageInfo = new SKImageInfo(10000, 10000);
 
             using SKPaint paint = new SKPaint
@@ -132,7 +168,7 @@ namespace SixLabors.ImageSharp.Drawing.Tests.Drawing
                 StrokeWidth = 1f,
                 IsAntialias = true,
             };
-            
+
             using SKSurface surface = SKSurface.Create(imageInfo);
             SKCanvas canvas = surface.Canvas;
             canvas.Clear(new SKColor(0,0, 0));
@@ -140,7 +176,7 @@ namespace SixLabors.ImageSharp.Drawing.Tests.Drawing
 
             string outDir = TestEnvironment.CreateOutputDirectory("Skia");
             string fn = System.IO.Path.Combine(outDir, $"Missisippi_Skia_{offset}.png");
-            
+
             using SKImage image = surface.Snapshot();
             using SKData data = image.Encode(SKEncodedImageFormat.Png, 100);
 
