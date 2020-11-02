3b70d7808c7cd998afa1eb003348ce3a008582f7
Anton Firszov <antonfir@gmail.com>
2020-11-02 12:52:49 +0100

    DrawCircleOutsideBoundsDrawingArea

--- tests/ImageSharp.Drawing.Tests/Issues/Issue_65.cs
+++ tests/ImageSharp.Drawing.Tests/Issues/Issue_65.cs
@@ -9,6 +9,7 @@ using Xunit;
 
 namespace SixLabors.ImageSharp.Drawing.Tests.Issues
 {
+    [GroupOutput("Drawing")]
     public class Issue_65
     {
         [Theory]
@@ -29,22 +30,25 @@ namespace SixLabors.ImageSharp.Drawing.Tests.Issues
             }
         }
 
+        public static TheoryData<int> DrawCircleOutsideBoundsDrawingArea_Data = new TheoryData<int>()
+        {
+            -110, -99, 0, 99, 110
+        };
+
         [Theory]
-        [InlineData(-110)] //Crash
-        [InlineData(-99)]  //Fine
-        [InlineData(99)]   //Fine
-        [InlineData(110)]  //Crash
-        public void DrawCircleOutsideBoundsDrawingArea(int xpos)
+        [WithSolidFilledImages(nameof(DrawCircleOutsideBoundsDrawingArea_Data), 100, 100, nameof(Color.Red), PixelTypes.Rgba32)]
+        public void DrawCircleOutsideBoundsDrawingArea(TestImageProvider<Rgba32> provider, int xpos)
         {
             int width = 100;
             int height = 100;
 
-            using (var image = new Image<Rgba32>(width, height, Color.Red))
-            {
-                var circle = new EllipsePolygon(xpos, 0, width, height);
+            using var image = provider.GetImage();
+            var circle = new EllipsePolygon(xpos, 0, width, height);
 
-                image.Mutate(x => x.Fill(Color.Black, circle));
-            }
+            provider.RunValidatingProcessorTest(x => x.Fill(Color.Black, circle),
+                $"xpos({xpos})",
+                appendPixelTypeToFileName: false,
+                appendSourceFileOrDescription: false);
         }
     }
 }
