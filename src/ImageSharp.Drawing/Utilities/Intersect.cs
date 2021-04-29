5a67308cd936e3e37ee9c98809233b75183801d0
Anton Firszov <antonfir@gmail.com>
2021-04-29 23:18:06 +0200

    Intersect.LineSegmentToLineSegment

---
+++ src/ImageSharp.Drawing/Utilities/Intersect.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Six Labors.
+// Licensed under the Apache License, Version 2.0.
+
+namespace SixLabors.ImageSharp.Drawing.Utilities
+{
+    internal static class Intersect
+    {
+        public static bool LineSegmentToLineSegment(PointF a0, PointF a1, PointF b0, PointF b1, ref PointF intersectionPoint)
+        {
+            float dax = a1.X - a0.X;
+            float day = a1.Y - a0.Y;
+            float dbx = b1.X - b0.X;
+            float dby = b1.Y - b0.Y;
+
+            float s = ((-day * (a0.X - b0.X)) + (dax * (a0.Y - b0.Y))) / ((-dbx * day) + (dax * dby));
+            float t = ((dbx * (a0.Y - b0.Y)) - (dby * (a0.X - b0.X))) / ((-dbx * day) + (dax * dby));
+
+            if (s >= 0 && s <= 1 && t >= 0 && t <= 1)
+            {
+                intersectionPoint.X = a0.X + (t * dax);
+                intersectionPoint.Y = a0.Y + (t * day);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ tests/ImageSharp.Drawing.Tests/Utilities/IntersectTests.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Six Labors.
+// Licensed under the Apache License, Version 2.0.
+
+using SixLabors.ImageSharp.Drawing.Utilities;
+using Xunit;
+
+namespace SixLabors.ImageSharp.Drawing.Tests.Drawing.Utils
+{
+    public class IntersectTests
+    {
+        public static TheoryData<PointF, PointF, PointF, PointF, PointF?> LineSegmentToLineSegment_Data =
+            new TheoryData<PointF, PointF, PointF, PointF, PointF?>()
+            {
+                { new PointF(0, 0), new PointF(2, 3), new PointF(1, 3), new PointF(1, 0), new PointF(1, 1.5f) },
+                { new PointF(0, 0), new PointF(2, 3), new PointF(1, 3), new PointF(1, 2), null },
+            };
+
+        [Theory]
+        [MemberData(nameof(LineSegmentToLineSegment_Data))]
+        public void LineSegmentToLineSegment(PointF a0, PointF a1, PointF b0, PointF b1, PointF? expected)
+        {
+            PointF ip = default;
+
+            bool result = Intersect.LineSegmentToLineSegment(a0, a1, b0, b1, ref ip);
+            Assert.Equal(result, expected.HasValue);
+            if (expected.HasValue)
+            {
+                Assert.Equal(expected.Value, ip);
+            }
+        }
+    }
+}
