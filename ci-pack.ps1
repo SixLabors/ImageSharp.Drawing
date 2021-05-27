a5041f046faef53eb32d88693a8d353d3f94e0e8
James Jackson-South <james_south@hotmail.com>
2021-05-27 17:22:54 +0100

    Try build first

--- ci-pack.ps1
+++ ci-pack.ps1
@@ -1,4 +1,5 @@
 $repositoryUrl = "https://github.com/$env:GITHUB_REPOSITORY"
 
 # Building for packing and publishing.
-dotnet pack -c Release --output "$PSScriptRoot/artifacts" /p:RepositoryUrl=$repositoryUrl
+dotnet build -c Release /p:RepositoryUrl=$repositoryUrl
+dotnet pack -c Release --no-build --output "$PSScriptRoot/artifacts" /p:RepositoryUrl=$repositoryUrl
