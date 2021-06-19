e1dc3f454ca14d8314229127d5d2c9f928dc7ea8
obayomy <obayomy@gmail.com>
2021-06-19 23:56:31 +0200

    Added wrapper for ClipperOffset

--- src/ImageSharp.Drawing/Shapes/OutlinePathExtensions.cs
+++ src/ImageSharp.Drawing/Shapes/OutlinePathExtensions.cs
@@ -3,10 +3,8 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Numerics;
-using System.Runtime.InteropServices;
-using ClipperLib;
+using SixLabors.ImageSharp.Drawing.Shapes.PolygonClipper;
 
 namespace SixLabors.ImageSharp.Drawing
 {
@@ -16,7 +14,6 @@ namespace SixLabors.ImageSharp.Drawing
     public static class OutlinePathExtensions
     {
         private const double MiterOffsetDelta = 20;
-        private const float ScalingFactor = 1000.0f;
 
         /// <summary>
         /// Generates a outline of the path with alternating on and off segments based on the pattern.
@@ -77,17 +74,10 @@ namespace SixLabors.ImageSharp.Drawing
                 return path.GenerateOutline(width, jointStyle: jointStyle);
             }
 
-            JoinType style = Convert(jointStyle);
-            EndType patternSectionCap = Convert(patternSectionCapStyle);
-
             IEnumerable<ISimplePath> paths = path.Flatten();
 
-            var offset = new ClipperOffset()
-            {
-                MiterLimit = MiterOffsetDelta
-            };
-
-            var buffer = new List<IntPoint>(3);
+            var offset = new ClipperOffset(MiterOffsetDelta);
+            var buffer = new List<PointF>();
             foreach (ISimplePath p in paths)
             {
                 bool online = !startOff;
@@ -116,13 +106,13 @@ namespace SixLabors.ImageSharp.Drawing
                         float t = targetLength / distToNext;
 
                         Vector2 point = (currentPoint * (1 - t)) + (targetPoint * t);
-                        buffer.Add(currentPoint.ToPoint());
-                        buffer.Add(point.ToPoint());
+                        buffer.Add(currentPoint);
+                        buffer.Add(point);
 
                         // we now inset a line joining
                         if (online)
                         {
-                            offset.AddPath(buffer, style, patternSectionCap);
+                            offset.AddPath(new ReadOnlySpan<PointF>(buffer.ToArray()), jointStyle, patternSectionCapStyle);
                         }
 
                         online = !online;
@@ -137,7 +127,7 @@ namespace SixLabors.ImageSharp.Drawing
                     }
                     else if (distToNext <= targetLength)
                     {
-                        buffer.Add(currentPoint.ToPoint());
+                        buffer.Add(currentPoint);
                         currentPoint = targetPoint;
                         i++;
                         targetLength -= distToNext;
@@ -148,16 +138,16 @@ namespace SixLabors.ImageSharp.Drawing
                 {
                     if (p.IsClosed)
                     {
-                        buffer.Add(points[0].ToPoint());
+                        buffer.Add(points[0]);
                     }
                     else
                     {
-                        buffer.Add(points[points.Length - 1].ToPoint());
+                        buffer.Add(points[points.Length - 1]);
                     }
 
                     if (online)
                     {
-                        offset.AddPath(buffer, style, patternSectionCap);
+                        offset.AddPath(new ReadOnlySpan<PointF>(buffer.ToArray()), jointStyle, patternSectionCapStyle);
                     }
 
                     online = !online;
@@ -168,7 +158,7 @@ namespace SixLabors.ImageSharp.Drawing
                 }
             }
 
-            return ExecuteOutliner(width, offset);
+            return offset.Execute(width);
         }
 
         /// <summary>
@@ -189,67 +179,10 @@ namespace SixLabors.ImageSharp.Drawing
         /// <returns>A new path representing the outline.</returns>
         public static IPath GenerateOutline(this IPath path, float width, JointStyle jointStyle = JointStyle.Square, EndCapStyle endCapStyle = EndCapStyle.Square)
         {
-            var offset = new ClipperOffset()
-            {
-                MiterLimit = MiterOffsetDelta
-            };
+            var offset = new ClipperOffset(MiterOffsetDelta);
+            offset.AddPath(path, jointStyle, endCapStyle);
 
-            JoinType style = Convert(jointStyle);
-            EndType openEndCapStyle = Convert(endCapStyle);
-
-            // Pattern can be applied to the path by cutting it into segments
-            IEnumerable<ISimplePath> paths = path.Flatten();
-            foreach (ISimplePath p in paths)
-            {
-                ReadOnlySpan<Vector2> vectors = MemoryMarshal.Cast<PointF, Vector2>(p.Points.Span);
-                var points = new List<IntPoint>(vectors.Length);
-                foreach (Vector2 v in vectors)
-                {
-                    points.Add(new IntPoint(v.X * ScalingFactor, v.Y * ScalingFactor));
-                }
-
-                EndType type = p.IsClosed ? EndType.etClosedLine : openEndCapStyle;
-
-                offset.AddPath(points, style, type);
-            }
-
-            return ExecuteOutliner(width, offset);
+            return offset.Execute(width);
         }
-
-        private static ComplexPolygon ExecuteOutliner(float width, ClipperOffset offset)
-        {
-            var tree = new List<List<IntPoint>>();
-            offset.Execute(ref tree, width * ScalingFactor / 2);
-            var polygons = new List<Polygon>();
-            foreach (List<IntPoint> pt in tree)
-            {
-                PointF[] points = pt.Select(p => new PointF(p.X / ScalingFactor, p.Y / ScalingFactor)).ToArray();
-                polygons.Add(new Polygon(new LinearLineSegment(points)));
-            }
-
-            return new ComplexPolygon(polygons.ToArray());
-        }
-
-        private static IntPoint ToPoint(this PointF vector)
-            => new IntPoint(vector.X * ScalingFactor, vector.Y * ScalingFactor);
-
-        private static IntPoint ToPoint(this Vector2 vector)
-            => new IntPoint(vector.X * ScalingFactor, vector.Y * ScalingFactor);
-
-        private static JoinType Convert(JointStyle style)
-            => style switch
-            {
-                JointStyle.Round => JoinType.jtRound,
-                JointStyle.Miter => JoinType.jtMiter,
-                _ => JoinType.jtSquare,
-            };
-
-        private static EndType Convert(EndCapStyle style)
-            => style switch
-            {
-                EndCapStyle.Round => EndType.etOpenRound,
-                EndCapStyle.Square => EndType.etOpenSquare,
-                _ => EndType.etOpenButt,
-            };
     }
 }
---
+++ src/ImageSharp.Drawing/Shapes/PolygonClipper/ClipperOffset.cs
@@ -0,0 +1,131 @@
+// Copyright (c) Six Labors.
+// Licensed under the Apache License, Version 2.0.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ClipperLib;
+
+namespace SixLabors.ImageSharp.Drawing.Shapes.PolygonClipper
+{
+    /// <summary>
+    /// Wrapper for clipper offset
+    /// </summary>
+    public class ClipperOffset
+    {
+        private const float ScalingFactor = 1000.0f;
+
+        private readonly ClipperLib.ClipperOffset innerClipperOffest;
+        private readonly object syncRoot = new object();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ClipperOffset"/> class.
+        /// </summary>
+        /// <param name="meterLimit">meter limit</param>
+        /// <param name="arcTolerance">arc tolerance</param>
+        public ClipperOffset(double meterLimit = 2, double arcTolerance = 0.25) => this.innerClipperOffest = new ClipperLib.ClipperOffset(meterLimit, arcTolerance);
+
+        /// <summary>
+        /// Calcualte Offset
+        /// </summary>
+        /// <param name="width">Width</param>
+        /// <returns>path offset</returns>
+        /// <exception cref="ClipperException">Execute: </exception>
+        public ComplexPolygon Execute(float width)
+        {
+            var tree = new List<List<IntPoint>>();
+            this.innerClipperOffest.Execute(ref tree, width * ScalingFactor / 2);
+            var polygons = new List<Polygon>();
+            foreach (List<IntPoint> pt in tree)
+            {
+                PointF[] points = pt.Select(p => new PointF(p.X / ScalingFactor, p.Y / ScalingFactor)).ToArray();
+                polygons.Add(new Polygon(new LinearLineSegment(points)));
+            }
+
+            return new ComplexPolygon(polygons.ToArray());
+        }
+
+        /// <summary>
+        /// Adds the path points
+        /// </summary>
+        /// <param name="pathPoints">The path points</param>
+        /// <param name="jointStyle">Joint Style</param>
+        /// <param name="endCapStyle">Endcap Style</param>
+        public void AddPath(ReadOnlySpan<PointF> pathPoints, JointStyle jointStyle, EndCapStyle endCapStyle) =>
+            this.AddPath(pathPoints, jointStyle, this.Convert(endCapStyle));
+
+        /// <summary>
+        /// Adds the path.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <param name="jointStyle">Joint Style</param>
+        /// <param name="endCapStyle">Endcap Style</param>
+        public void AddPath(IPath path, JointStyle jointStyle, EndCapStyle endCapStyle)
+        {
+            if (path is null)
+            {
+                throw new ArgumentNullException(nameof(path));
+            }
+
+            foreach (ISimplePath p in path.Flatten())
+            {
+                this.AddPath(p, jointStyle, endCapStyle);
+            }
+        }
+
+        /// <summary>
+        /// Adds the path.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <param name="jointStyle">Joint Style</param>
+        /// <param name="endCapStyle">Endcap Style</param>
+        private void AddPath(ISimplePath path, JointStyle jointStyle, EndCapStyle endCapStyle)
+        {
+            ReadOnlySpan<PointF> vectors = path.Points.Span;
+            EndType type = path.IsClosed ? EndType.etClosedLine : this.Convert(endCapStyle);
+            this.AddPath(vectors, jointStyle, type);
+        }
+
+        /// <summary>
+        /// Adds the path.
+        /// </summary>
+        /// <param name="pathPoints">The path points</param>
+        /// <param name="jointStyle">Joint Style</param>
+        /// <param name="endCapStyle">Endcap Style</param>
+        /// <exception cref="ClipperException">AddPath: Open paths have been disabled.</exception>
+        private void AddPath(ReadOnlySpan<PointF> pathPoints, JointStyle jointStyle, EndType endCapStyle)
+        {
+            var points = new List<IntPoint>();
+            foreach (PointF v in pathPoints)
+            {
+                if (float.IsNaN(v.X) || float.IsNaN(v.Y))
+                {
+                    throw new ClipperException("Invalid Point");
+                }
+
+                points.Add(new IntPoint(v.X * ScalingFactor, v.Y * ScalingFactor));
+            }
+
+            lock (this.syncRoot)
+            {
+                this.innerClipperOffest.AddPath(points, this.Convert(jointStyle), endCapStyle);
+            }
+        }
+
+        private JoinType Convert(JointStyle style)
+        => style switch
+        {
+            JointStyle.Round => JoinType.jtRound,
+            JointStyle.Miter => JoinType.jtMiter,
+            _ => JoinType.jtSquare,
+        };
+
+        private EndType Convert(EndCapStyle style)
+        => style switch
+        {
+            EndCapStyle.Round => EndType.etOpenRound,
+            EndCapStyle.Square => EndType.etOpenSquare,
+            _ => EndType.etOpenButt,
+        };
+    }
+}
