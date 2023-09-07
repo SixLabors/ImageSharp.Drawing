ac69b4cf92c79c6476307c52a766705f780233c5
James Jackson-South <james_south@hotmail.com>
2023-09-07 21:11:04 +1000

    Tweak tolerance for Win .NET 7

--- tests/ImageSharp.Drawing.Tests/Drawing/FillPolygonTests.cs
+++ tests/ImageSharp.Drawing.Tests/Drawing/FillPolygonTests.cs
@@ -196,7 +196,7 @@ public class FillPolygonTests
 
             provider.RunValidatingProcessorTest(
                 c => c.Fill(Color.DeepPink, circle).Fill(Color.LightGray, star).Fill(Color.ForestGreen, shape),
-                comparer: ImageComparer.TolerantPercentage(0.014f),
+                comparer: ImageComparer.TolerantPercentage(0.0203F),
                 testOutputDetails: operation.ToString(),
                 appendSourceFileOrDescription: false,
                 appendPixelTypeToFileName: false);
