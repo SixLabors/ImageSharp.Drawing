a7481bb4cbe160a02ab8793ef46b87d4a260db2a
James Jackson-South <james_south@hotmail.com>
2023-09-08 12:16:22 +1000

    Revert tolerance changes

--- tests/ImageSharp.Drawing.Tests/Drawing/FillPolygonTests.cs
+++ tests/ImageSharp.Drawing.Tests/Drawing/FillPolygonTests.cs
@@ -196,7 +196,7 @@ public class FillPolygonTests
 
             provider.RunValidatingProcessorTest(
                 c => c.Fill(Color.DeepPink, circle).Fill(Color.LightGray, star).Fill(Color.ForestGreen, shape),
-                comparer: ImageComparer.TolerantPercentage(0.0287F),
+                comparer: ImageComparer.TolerantPercentage(0.01F),
                 testOutputDetails: operation.ToString(),
                 appendSourceFileOrDescription: false,
                 appendPixelTypeToFileName: false);
