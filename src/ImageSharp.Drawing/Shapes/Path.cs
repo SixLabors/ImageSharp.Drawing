975c45358f073c5efa79b624e72e145b4e2c1b6b
TechPizza <23627133+TechPizzaDev@users.noreply.github.com>
2024-05-03 14:01:31 +0200

    Derive EllipsePolygon from Polygon

--- src/ImageSharp.Drawing/Shapes/Path.cs
+++ src/ImageSharp.Drawing/Shapes/Path.cs
@@ -88,7 +88,7 @@ public class Path : IPath, ISimplePath, IPathInternals, IInternalPathOwner
     /// </summary>
     internal bool RemoveCloseAndCollinearPoints { get; set; } = true;
 
-    private InternalPath InnerPath =>
+    private protected InternalPath InnerPath =>
         this.innerPath ??= new InternalPath(this.lineSegments, this.IsClosed, this.RemoveCloseAndCollinearPoints);
 
     /// <inheritdoc />
