0bfb3c0486e75fc6c97d2e3110648f63edf9a55f
James Jackson-South <james_south@hotmail.com>
2026-03-04 11:10:21 +1000

    Mode DrawComplexPolygonTests

--- tests/ImageSharp.Drawing.Tests/Processing/ProcessWithDrawingCanvasTests.Primitives.cs
+++ tests/ImageSharp.Drawing.Tests/Processing/ProcessWithDrawingCanvasTests.Primitives.cs
@@ -47,6 +47,7 @@ public partial class ProcessWithDrawingCanvasTests
             appendPixelTypeToFileName: false,
             appendSourceFileOrDescription: false);
         image.CompareToReferenceOutput(
+            ImageComparer.TolerantPercentage(0.001F),
             provider,
             testDetails,
             appendPixelTypeToFileName: false,
@@ -216,6 +217,65 @@ public partial class ProcessWithDrawingCanvasTests
         });
 
         DrawLinesImpl(provider, colorName, alpha, thickness, antialias, pen);
+    }
+
+    [Theory]
+    [WithBasicTestPatternImages(250, 350, PixelTypes.Rgba32, false, false, false)]
+    [WithBasicTestPatternImages(250, 350, PixelTypes.Rgba32, true, false, false)]
+    [WithBasicTestPatternImages(250, 350, PixelTypes.Rgba32, false, true, false)]
+    [WithBasicTestPatternImages(250, 350, PixelTypes.Rgba32, false, false, true)]
+    public void DrawComplexPolygon<TPixel>(TestImageProvider<TPixel> provider, bool overlap, bool transparent, bool dashed)
+        where TPixel : unmanaged, IPixel<TPixel>
+    {
+        Polygon simplePath = new(new LinearLineSegment(
+            new Vector2(10, 10),
+            new Vector2(200, 150),
+            new Vector2(50, 300)));
+
+        Polygon hole1 = new(new LinearLineSegment(
+            new Vector2(37, 85),
+            overlap ? new Vector2(130, 40) : new Vector2(93, 85),
+            new Vector2(65, 137)));
+
+        IPath clipped = simplePath.Clip(hole1);
+
+        Color color = Color.White;
+        if (transparent)
+        {
+            color = color.WithAlpha(150 / 255F);
+        }
+
+        string testDetails = string.Empty;
+        if (overlap)
+        {
+            testDetails += "_Overlap";
+        }
+
+        if (transparent)
+        {
+            testDetails += "_Transparent";
+        }
+
+        if (dashed)
+        {
+            testDetails += "_Dashed";
+        }
+
+        Pen pen = dashed ? Pens.Dash(color, 5F) : Pens.Solid(color, 5F);
+        DrawingOptions options = new();
+
+        using Image<TPixel> image = provider.GetImage();
+        image.Mutate(ctx => ctx.ProcessWithCanvas(options, canvas => canvas.Draw(pen, clipped)));
+        image.DebugSave(
+            provider,
+            testDetails,
+            appendPixelTypeToFileName: false,
+            appendSourceFileOrDescription: false);
+        image.CompareToReferenceOutput(
+            provider,
+            testDetails,
+            appendPixelTypeToFileName: false,
+            appendSourceFileOrDescription: false);
     }
 
     private static void DrawLinesImpl<TPixel>(
